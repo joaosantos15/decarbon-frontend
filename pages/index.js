c6370db2bb76a3e357ddc23020cb2ffa90516515
Joao Santos <jrmsantos15@gmail.com>
2022-04-24 00:26:25 +0200

    fix build

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { MainPage } from './MainPage'
+import { MainPage } from '../components/MainPage'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
